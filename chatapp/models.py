08f0ca5e1c715cd6adb9511888c39b50a3d5fc54
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2024-03-31 12:22:04 +0530

    created the models for chat and room

--- chatapp/models.py
+++ chatapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Model for Room
+class Room(models.Model):
+    room_name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.room_name
+    
+
+
+# Model for Message
+class Message(models.Model):
+    room = models.ForeignKey(Room,on_delete=models.CASCADE)
+    sender = models.CharField(max_length=255)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return str(self.room)
+    
+    
