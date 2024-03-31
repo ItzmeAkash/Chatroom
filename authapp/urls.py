4b1f4e561f10f501e3f2793b9c0119ff23b92a5c
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2024-03-31 11:38:46 +0530

    modifed the views

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import SignUpView,loginView,home
+from .views import SignUpView,home
 from django.views.generic.base import TemplateView 
 
 
--- authapp/views.py
+++ authapp/views.py
@@ -1,37 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login,authenticate
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 
 @login_required
 def home(request):
-    return render(request, 'home.html', {})
+    return render(request, 'home.html')
 
-# def signupview(request):
-#     if request.method == 'POST':
-#          form = UserCreationForm(request.POST)
-#          if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password')
-#             user = authenticate(username=username, password=password)
-#             login(request, user)
-#             return redirect("home")
-#     else:
-#         form = UserCreationForm()
-#     return render(request, "registration/signup.html", {"form": form})
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
-    
-def loginView(request):
-    return render(request, "registration/login.html")
-
-
-def signupView(request):
-    return render(request, "registration/signup.html")
