48db7b7e2c809b2b61d145403fd5ba77f10f04a6
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2024-03-31 11:21:32 +0530

    login signup added

---
+++ authapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,include
+from .views import SignUpView,loginView,home
+from django.views.generic.base import TemplateView 
+
+
+urlpatterns = [
+    path("",home, name="home"),
+    path("accounts/signup/", SignUpView.as_view(), name="signup"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    
+]
